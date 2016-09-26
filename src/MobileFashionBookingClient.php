9368c104197ac3a1193af6dba0d381d13a81ac9e
TaeL Kim <tael.programmer@gmail.com>
2016-09-26 10:57:20 +0900

    1sec

--- src/MobileFashionBookingClient.php
+++ src/MobileFashionBookingClient.php
@@ -46,7 +46,7 @@ class MobileFashionBookingClient
 
     public function waitOpenTime()
     {
-        $second = new \DateInterval('PT2S'); // 2 sec
+        $second = new \DateInterval('PT1S'); // 1 sec
         $nosp = new ServerTime(new Client());
         $serverTime = $nosp->getServerDateTime('http://nosp.da.naver.com');
 
